3bc40c12c5c96c885fd0cd8ed94eabc061638331
markhabat <msansyzbay@gmail.com>
2022-11-12 20:45:23 +0600

    add route and controller

--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo 'Hello Admin';
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\Http\Controllers;
+use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
+$twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
+$app  = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+$app->add(TwigMiddleware::create($app, $twig));
+
+$app->get('/', Controllers\IndexController::class . ':home');
+
+$app->run();
