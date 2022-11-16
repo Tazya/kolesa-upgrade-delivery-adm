d2a4bfb90bf49b58871949fae04039601f8c18df
Koala610 <vladli2000610@gmail.com>
2022-11-16 15:18:57 +0600

    edit: logic of sendMessages function to connect it with microservice

--- src/Http/Controllers/MessageController.php
+++ src/Http/Controllers/MessageController.php
@@ -5,9 +5,11 @@ use App\Model\Validators\MessageValidator;
 use Slim\Http\ServerRequest;
 use Slim\Http\Response;
 use Slim\Views\Twig;
+use GuzzleHttp\Client;
 
 class MessageController
 {
+   const API_URI = "localhost:8080"; 
     public function index(ServerRequest $request, Response $response)
     {
         $view = Twig::fromRequest($request);
@@ -28,6 +30,20 @@ class MessageController
                 "errors" => $errors,
             ]);
         }
-        return $view->render($response, 'message/index.twig', ["message" => $messageData]);
+        $serviceClient = new Client([
+            'base_uri' => self::API_URI,
+            'timeout' => 2.0,
+        ]);
+        $serviceResult = $serviceClient->request('POST', '/messages/sendToAll', ['json' => $messageData]);
+        $body = $serviceResult->getBody();
+        $resultArray = json_decode($body, true);
+        if(!array_key_exists("status", $resultArray)){
+            return $response->write("Wrong data format");
+        }
+        if($resultArray["status"]==="error"){
+            return $response->write("Error: " . $resultArray["error"]);
+        }
+        return $response->withHeader('Content-Type', 'application/json')->write($body);
     }
+
 }
