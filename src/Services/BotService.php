068a3629ad4b08de094cb7a7aaeb99494c9303ca
markhabat <msansyzbay@gmail.com>
2022-11-23 19:38:48 +0600

    fix route

--- src/Services/BotService.php
+++ src/Services/BotService.php
@@ -3,11 +3,9 @@
 namespace App\Services;
 
 use GuzzleHttp\ClientInterface;
-use Yosymfony\Toml\Toml;
 
 class BotService
 {
-    private const CONFIG_PATH = '../config/config.toml';
     private $client;
 
     public function __construct(ClientInterface $client)
@@ -17,13 +15,11 @@ class BotService
 
     public function sendToAll(string $title, string $body)
     {
-        $cfg = Toml::ParseFile(self::CONFIG_PATH);
-
         $messageData = [
             "title" => $title,
             "body" => $body
         ];
 
-        return $this->client->request('POST', $cfg['bot_service_addr'] . '/messages/sendToAll', ['json' => $messageData]);
+        return $this->client->request('POST', '/messages/sendToAll', ['json' => $messageData]);
     }
 }
