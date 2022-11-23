3b418b394df71eea2756025fe3466fc7057f6171
markhabat <msansyzbay@gmail.com>
2022-11-23 17:01:44 +0600

    add bot service addr to config and move service folder

--- src/Services/BotService.php
+++ src/Services/BotService.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace App\Model\Services;
+namespace App\Services;
 
 use GuzzleHttp\ClientInterface;
+use Yosymfony\Toml\Toml;
 
 class BotService
 {
-    const ADDR = 'localhost:8888';
+    private const CONFIG_PATH = '../config/config.toml';
     private $client;
 
     public function __construct(ClientInterface $client)
@@ -16,11 +17,13 @@ class BotService
 
     public function sendToAll(string $title, string $body)
     {
+        $cfg = Toml::ParseFile(self::CONFIG_PATH);
+
         $messageData = [
             "title" => $title,
             "body" => $body
         ];
 
-        return $this->client->request('POST', self::ADDR . '/messages/sendToAll', ['json' => $messageData]);
+        return $this->client->request('POST', $cfg['bot_service_addr'] . '/messages/sendToAll', ['json' => $messageData]);
     }
 }
